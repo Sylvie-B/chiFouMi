8eb23ff3f27631f4b7ea876ed70efdf60c695f21
Sylvie-B <unsolo@hotmail.fr>
2020-12-16 15:21:15 +0100

    reset listener

--- appCFM.js
+++ appCFM.js
@@ -1,32 +1,52 @@
 // get buttons
 let stone = document.getElementById('stone');
-
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 
+//  get the different choices
+let choice = document.getElementById('userItem').getElementsByTagName('div');
+
 // get display game
 let choiceC = document.getElementById('computerChoice');
+choiceC.style.fontSize = '10vw';
 let choiceU = document.getElementById('userChoice');
 choiceU.style.fontSize = '10vw';
-
 
 // get display history
 
 // get display point
 let point = document.getElementById('point');
 
-// listen buttons
+// listen buttons   // random // test // info
 stone.addEventListener('click', function (){
     choiceU.innerHTML = stone.innerHTML;
-    randomChoice();
+    randomChoice(stone);
 });
 
-// function : randon choice computer
-let choice = document.getElementById('choice').getElementsByTagName('div');
+paper.addEventListener('click', function (){
+    choiceU.innerHTML = paper.innerHTML;
+    randomChoice(paper);
+});
 
-function randomChoice (){
+scissors.addEventListener('click', function (){
+    choiceU.innerHTML = paper.innerHTML;
+    randomChoice(scissors);
+})
+
+// function : randon choice computer
+function randomChoice (item){
     let x = Math.round(Math.random() * (choice.length-1));
+    choiceC.innerHTML = choice[x].innerHTML;
+    // test
+    if (x === 0)
+        switch (item){
+            case stone :
+                // match null
+                break;
+            case paper :
+                //
+        }
+
 }
 
-// test
 
